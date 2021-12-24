d20d1ff347d3fd4682c2d50f5af16d07d759ee90
emb-yogeshc <emb-yogeshc@emb-ban-606.embitel.local>
2021-12-24 08:28:49 +0530

    Sending data in excel instead of Hardcoding to ValidLogin

--- src/test/java/scripts/ValidLogin.java
+++ src/test/java/scripts/ValidLogin.java
@@ -4,24 +4,32 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import generic.BaseTest;
+import generic.Excel;
 import pages.ETTPage;
 import pages.LoginPage;
 
 public class ValidLogin extends BaseTest {
 	
-	@Test(priority = 1,groups="smoke")
+	@Test(priority = 1,groups={"smoke","login"})
 	public void testValidLogin() {
+		 String un=Excel.getData(XL_PATH, "ValidLogin", 1, 0);
+		 String pw=Excel.getData(XL_PATH, "ValidLogin", 1, 1);
+		 String failMSG=Excel.getData(XL_PATH, "ValidLogin", 1, 2);
+		 
 //	    1. Enter Valid User Name
 		LoginPage loginPage=new LoginPage(driver);
-		loginPage.setUserName("admin");
+		loginPage.setUserName(un);
+		
 //	    2. Enter Valid Password
-		loginPage.setPassword("manager");
+		loginPage.setPassword(pw);
+		
 //	    3. click on login button
 		loginPage.clickLoginButton();
+		
 //	    4. Verify that Home Page is Displayed
 		ETTPage ettPage=new ETTPage(driver);
 		boolean result = ettPage.verifyWelcomeIsDisplayed(wait);
-		Assert.assertTrue(result,"Home Page is not displayed");
+		Assert.assertTrue(result,failMSG);
 	}
 
 }
